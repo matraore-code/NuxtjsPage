b547c9d5e23aa7fdec360a897c422678341a927e
mohamedamine456 <aminelachheb456@gmail.com>
2021-10-08 17:17:14 +0100

    add post request

--- .nuxt/index.js
+++ .nuxt/index.js
@@ -12,7 +12,7 @@ import { setContext, getLocation, getRouteData, normalizeError } from './utils'
 
 /* Plugins */
 
-import nuxt_plugin_plugin_2f6fbfb6 from 'nuxt_plugin_plugin_2f6fbfb6' // Source: ./components/plugin.js (mode: 'all')
+import nuxt_plugin_plugin_16c37208 from 'nuxt_plugin_plugin_16c37208' // Source: ./components/plugin.js (mode: 'all')
 
 // Component: <ClientOnly>
 Vue.component(ClientOnly.name, ClientOnly)
@@ -176,8 +176,8 @@ async function createApp(ssrContext, config = {}) {
   }
   // Plugin execution
 
-  if (typeof nuxt_plugin_plugin_2f6fbfb6 === 'function') {
-    await nuxt_plugin_plugin_2f6fbfb6(app.context, inject)
+  if (typeof nuxt_plugin_plugin_16c37208 === 'function') {
+    await nuxt_plugin_plugin_16c37208(app.context, inject)
   }
 
   // Lock enablePreview in context
--- .nuxt/router.js
+++ .nuxt/router.js
@@ -4,14 +4,14 @@ import { normalizeURL, decode } from 'ufo'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _0632dcc4 = () => interopDefault(import('../pages/About.vue' /* webpackChunkName: "pages/About" */))
-const _475e9de0 = () => interopDefault(import('../pages/coming.vue' /* webpackChunkName: "pages/coming" */))
-const _e84d4f98 = () => interopDefault(import('../pages/Confirmation.vue' /* webpackChunkName: "pages/Confirmation" */))
-const _60057671 = () => interopDefault(import('../pages/Contact.vue' /* webpackChunkName: "pages/Contact" */))
-const _75a16c23 = () => interopDefault(import('../pages/form.vue' /* webpackChunkName: "pages/form" */))
-const _04c3479a = () => interopDefault(import('../pages/profile.vue' /* webpackChunkName: "pages/profile" */))
-const _22edf967 = () => interopDefault(import('../pages/Search.vue' /* webpackChunkName: "pages/Search" */))
-const _b5a766fa = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
+const _4eeb8f9b = () => interopDefault(import('../pages/About.vue' /* webpackChunkName: "pages/About" */))
+const _35f95f83 = () => interopDefault(import('../pages/coming.vue' /* webpackChunkName: "pages/coming" */))
+const _3f91d8d2 = () => interopDefault(import('../pages/Confirmation.vue' /* webpackChunkName: "pages/Confirmation" */))
+const _44c2e92e = () => interopDefault(import('../pages/Contact.vue' /* webpackChunkName: "pages/Contact" */))
+const _36363a86 = () => interopDefault(import('../pages/form.vue' /* webpackChunkName: "pages/form" */))
+const _2cfe8b52 = () => interopDefault(import('../pages/profile.vue' /* webpackChunkName: "pages/profile" */))
+const _1188bb0a = () => interopDefault(import('../pages/Search.vue' /* webpackChunkName: "pages/Search" */))
+const _119d6b00 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 
 const emptyFn = () => {}
 
@@ -26,35 +26,35 @@ export const routerOptions = {
 
   routes: [{
     path: "/About",
-    component: _0632dcc4,
+    component: _4eeb8f9b,
     name: "About"
   }, {
     path: "/coming",
-    component: _475e9de0,
+    component: _35f95f83,
     name: "coming"
   }, {
     path: "/Confirmation",
-    component: _e84d4f98,
+    component: _3f91d8d2,
     name: "Confirmation"
   }, {
     path: "/Contact",
-    component: _60057671,
+    component: _44c2e92e,
     name: "Contact"
   }, {
     path: "/form",
-    component: _75a16c23,
+    component: _36363a86,
     name: "form"
   }, {
     path: "/profile",
-    component: _04c3479a,
+    component: _2cfe8b52,
     name: "profile"
   }, {
     path: "/Search",
-    component: _22edf967,
+    component: _1188bb0a,
     name: "Search"
   }, {
     path: "/",
-    component: _b5a766fa,
+    component: _119d6b00,
     name: "index"
   }],
 
