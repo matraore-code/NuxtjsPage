0c7db3848815a4a5b7c4454c5c82a00799bdec27
Mohamed amine Lachheb <mlachheb@s2r1p1.1337.ma>
2021-10-10 18:19:14 +0100

    mail database

--- B-Carte__backend/controllers/mailer-controller.js
+++ B-Carte__backend/controllers/mailer-controller.js
@@ -3,36 +3,32 @@ const { validationResult } = require('express-validator');
 
 const HttpError = require('../models/http-error');
 
+const Mail = require('../models/mail');
+
 const sendEmail = async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        const error = new HttpError("Invalid Inputs, Please check Your Data!", 442);
-        return next(error);
+      const error = new HttpError("Invalid Inputs, Please check Your Data!", 442);
+      return next(error);
     }
-    let testAccount = await nodemailer.createTestAccount();
-
-    let transporter = nodemailer.createTransport({
-        host: "smtp.ethereal.email",
-        port: 587,
-        secure: false, 
-        auth: {
-          user: testAccount.user,
-          pass: testAccount.pass,
-        },
-      });
     
-      let info = await transporter.sendMail({
-        from: '<foo@example.com>',
-        to: "aminelachheb456@gmail.com",
-        subject: "Message From Contact Page:", // Subject line
-        text: req.body.message, // plain text body
-        html: `<b>${req.body.message}</b>`, // html body
-      });
-    
-      console.log("Message sent: %s", info.messageId);
+    const { name, surname, email, telephone, profession, message } = req.body;
 
-      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    const createdMail = new Mail({
+      name: name,
+      surname: surname,
+      email: email,
+      telephone: telephone || '',
+      profession: profession || '',
+      message: message
+    });
 
+    try {
+      await createdMail.save();
+    } catch (err) {
+      const error = new HttpError("Creating Mail Failed, Please try again!", 500);
+      return next(error);
+    }
     res.status(200).json({ message: "Message Send With Success." });
 };
 
